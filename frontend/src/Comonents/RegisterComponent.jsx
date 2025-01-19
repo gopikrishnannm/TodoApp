01054e27d27d5b9ce521dd35423466aa3e650f76
gopikrishnannm <gopikrishnannm01@gmail.com>
2025-01-19 14:43:08 +0530

    validating register user

--- frontend/src/Comonents/RegisterComponent.jsx
+++ frontend/src/Comonents/RegisterComponent.jsx
@@ -1,4 +1,4 @@
-import { Formik, fieldset, Field, Form} from "formik"
+import { Formik, fieldset, Field, Form, ErrorMessage} from "formik"
 import { isUserExist, register } from "../BasicComponents/Apiforlogin"
 import { useState } from "react"
 export default function RegisterComponent(){
@@ -41,6 +41,17 @@ export default function RegisterComponent(){
         }
     }
 
+    function validate(values){
+        const error = {}
+        if(values.username.length<=4){
+            error.username = "enter a 5 or more character username"
+        }
+        if(values.password.length<=5){
+            error.password= "enter a 5 or more length password"
+        }
+        return error
+    }
+
     return(
 
         
@@ -57,12 +68,26 @@ export default function RegisterComponent(){
                 }
             }
             onSubmit={onSubmit}
+
+            validate={validate}
+
+            validateOnChange={false}
+            validateOnBlur={false}
             
             >    
                 {
 
                     (props) =>(
                         <Form>
+                        
+                            <ErrorMessage name="username" component="div"/>
+
+                                
+                            
+                            <ErrorMessage name="password" component="div"/>
+                        
+                        
+                        
                             <fieldset>
                                 <label htmlFor="username">Username</label>
                                 <Field type="text" name="username"/>
